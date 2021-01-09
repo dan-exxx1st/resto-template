6b52a197f50ca51440223cdbe6166d5f8232da14
Daniil Parfenov <dan.exxx1st@gmail.com>
2021-01-09 20:20:22 +0300

    Fix build script.
    Move main page sections in separate folder tso that they do not fall into build folder.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Resto
+
 Template based on psd template which was be found on the internet space. This template was be created by pug, sass (scss), js and webpack 4 (for building app);
+
+[Link to psd template](https://drive.google.com/file/d/1Tl640HputF_ud7cnEUV_K8VezxEl5EFc/view?usp=sharing)
--- webpack/webpack.base.js
+++ webpack/webpack.base.js
@@ -3,31 +3,30 @@ const fs = require('fs');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { VueLoaderPlugin } = require('vue-loader');
 
 const PATHS = {
 	src: path.join(__dirname, '../src'),
-	dist: path.join(__dirname, '../dist'),
-	assets: 'assets/',
+	dist: path.join(__dirname, '../build'),
+	assets: 'assets/'
 };
 
 const PAGES_DIR = `${PATHS.src}/views`;
 const PAGES = fs
 	.readdirSync(PAGES_DIR)
-	.filter((fileName) => fileName.endsWith('.pug'));
+	.filter(fileName => fileName.endsWith('.pug'));
 
 module.exports = {
 	// BASE config
 	externals: {
-		paths: PATHS,
+		paths: PATHS
 	},
 	entry: {
-		app: PATHS.src,
+		app: PATHS.src
 	},
 	output: {
 		filename: `${PATHS.assets}js/[name].[hash].js`,
 		path: PATHS.dist,
-		publicPath: '/',
+		publicPath: '/'
 	},
 	optimization: {
 		splitChunks: {
@@ -36,54 +35,39 @@ module.exports = {
 					name: 'vendors',
 					test: /node_modules/,
 					chunks: 'all',
-					enforce: true,
-				},
-			},
-		},
+					enforce: true
+				}
+			}
+		}
 	},
 	module: {
 		rules: [
 			{
 				test: /\.pug$/,
 				oneOf: [
-					// this applies to <template lang="pug"> in Vue components
 					{
-						resourceQuery: /^\?vue/,
-						use: ['pug-plain-loader'],
-					},
-					// this applies to pug imports inside JavaScript
-					{
-						use: ['pug-loader'],
-					},
-				],
+						use: ['pug-loader']
+					}
+				]
 			},
 			{
 				test: /\.js$/,
 				loader: 'babel-loader',
-				exclude: '/node_modules/',
-			},
-			{
-				test: /\.vue$/,
-				loader: 'vue-loader',
-				options: {
-					loader: {
-						scss: 'vue-style-loader!css-loader!sass-loader',
-					},
-				},
+				exclude: '/node_modules/'
 			},
 			{
 				test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
 				loader: 'file-loader',
 				options: {
-					name: '[name].[ext]',
-				},
+					name: '[name].[ext]'
+				}
 			},
 			{
 				test: /\.(png|jpg|gif|svg)$/,
 				loader: 'file-loader',
 				options: {
-					name: '[name].[ext]',
-				},
+					name: '[name].[ext]'
+				}
 			},
 			{
 				test: /\.scss$/,
@@ -92,20 +76,20 @@ module.exports = {
 					MiniCssExtractPlugin.loader,
 					{
 						loader: 'css-loader',
-						options: { sourceMap: true },
+						options: { sourceMap: true }
 					},
 					{
 						loader: 'postcss-loader',
 						options: {
 							sourceMap: true,
-							config: { path: `./postcss.config.js` },
-						},
+							config: { path: `./postcss.config.js` }
+						}
 					},
 					{
 						loader: 'sass-loader',
-						options: { sourceMap: true },
-					},
-				],
+						options: { sourceMap: true }
+					}
+				]
 			},
 			{
 				test: /\.css$/,
@@ -114,39 +98,38 @@ module.exports = {
 					MiniCssExtractPlugin.loader,
 					{
 						loader: 'css-loader',
-						options: { sourceMap: true },
+						options: { sourceMap: true }
 					},
 					{
 						loader: 'postcss-loader',
 						options: {
 							sourceMap: true,
-							config: { path: `./postcss.config.js` },
-						},
-					},
-				],
-			},
-		],
+							config: { path: `./postcss.config.js` }
+						}
+					}
+				]
+			}
+		]
 	},
 	resolve: {
 		alias: {
-			'~': PATHS.src,
-		},
+			'~': PATHS.src
+		}
 	},
 	plugins: [
 		new MiniCssExtractPlugin({
-			filename: `${PATHS.assets}css/[name].[hash].css`,
+			filename: `${PATHS.assets}css/[name].[hash].css`
 		}),
 		new CopyWebpackPlugin([
-			{ from: `${PATHS.src}/${PATHS.assets}img`, to: `${PATHS.assets}img` },
-			{ from: `${PATHS.src}/${PATHS.assets}fonts`, to: `${PATHS.assets}fonts` },
+			{ from: `${PATHS.src}/${PATHS.assets}img`, to: `${PATHS.assets}img` }
 		]),
 
 		...PAGES.map(
-			(page) =>
+			page =>
 				new HtmlWebpackPlugin({
 					template: `${PAGES_DIR}/${page}`,
-					filename: `./${page.replace(/\.pug/, '.html')}`,
+					filename: `./${page.replace(/\.pug/, '.html')}`
 				})
-		),
-	],
+		)
+	]
 };
